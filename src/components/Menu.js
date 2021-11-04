aebae0869e0e86629bc1f96c0672af84f5c583ed
Henna Virnes <t9vihe00@students.oamk.fi>
2021-11-04 09:16:24 +0200

    initial build and some setup

---
+++ src/components/Menu.js
@@ -0,0 +1,23 @@
+import React from "react";
+import styles from './menu.module.css';
+
+
+
+export default function menu(props) {
+  return (
+    <>
+      <div className={styles.menu}> 
+        <img alt='menu icon' className = {styles.hamIcon}></img>
+        <div className = {styles.menuTitle}>Hyvinvoiva maaseutu</div>
+        <div className={styles.icon}> icon </div>
+      </div>
+      <div className={styles.sections}>
+        <div className={styles.sec1}>section 1</div>
+        <div className={styles.sec2}>section 2</div>
+        <div className={styles.sec3}>section 3</div>
+        <div className={styles.sec4}>section 4</div>
+      </div>
+    </>
+  )
+}
+
