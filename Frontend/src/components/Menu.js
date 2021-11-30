53352457ef85e258a2ce865f75ccc2f7effb1437
Felix <t9pafe00@students.oamk.fi>
2021-11-30 08:24:13 +0200

    added delete boxes

--- Frontend/src/components/Menu.js
+++ Frontend/src/components/Menu.js
@@ -4,14 +4,20 @@ import MenuIcon from '@material-ui/icons/Menu';
 import AccountCircleIcon from '@material-ui/icons/AccountCircle';
 import Settings from '@material-ui/icons/Settings';
 import LoginModal from "./LoginModal";
+import DeleteAccount from "./DeleteAccount";
 
 export default function Menu(props) {
 
   const [content, display] = useState("");
+  const [DeleteBox, remove] = useState("");
 
   //Removing the login object on screen when pressing x
   function hideLogin(){
     display("");
+  }
+
+  function hideDelete(){
+    remove("");
   }
 
   return (
@@ -19,7 +25,10 @@ export default function Menu(props) {
       <div className={styles.menu}> 
         <MenuIcon className={styles.icons}/>
         <div className = {styles.menuTitle}>Hyvinvoiva maaseutu</div>
-        <Settings className={ props.loggedIn ? styles.settings : styles.scale0 } />
+        
+        <Settings className={ props.loggedIn ? styles.settings : styles.scale0 } 
+                  onClick={()=> display(<DeleteAccount hideDelete={() => hideDelete()} />)} />
+
         <AccountCircleIcon className={ props.loggedIn ? styles.scale0 : styles.icons2 }
                            onClick={()=> display(<LoginModal pass={props.pass} user={props.user} conf={props.conf}
                            updateConf={props.updateConf} updatePass={props.updatePass} updateUser={props.updateUser} 
@@ -27,6 +36,7 @@ export default function Menu(props) {
         
       </div>
       {content}
+      {DeleteBox}
     </>
   )
 }
