83e9540005ff76dd760bee317b72c349d8f8162a
Felix <t9pafe00@students.oamk.fi>
2021-12-07 11:07:27 +0200

    puzzle image styling

--- Frontend/src/components/section4.js
+++ Frontend/src/components/section4.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 
 import styles from './sectionscontainer.module.css'
 import anotherStyle from './sectioncomponents/linkbutton.module.css'
+import imageStyle from './sectioncomponents/imgcontainer.module.css'
 import Title from './sectioncomponents/Title.js';
 import Caption from './sectioncomponents/Caption.js';
 import OsaAlueet from './pictures/osa-alueet2.png';
@@ -23,8 +24,9 @@ export default function Section4() {
         <Title title='Hyvinvoinnin osa-alueet'/>
         <Caption text ='Ihminen on kokonaisuus, jonka hyvinvointi koostuu monesta eri asiasta. Kokonaisvaltaista hyvinvointia voidaan tarkastella neljän eri ulottuvuuden näkökulmasta: fyysinen, psyykkinen, sosiaalinen ja taloudellinen hyvinvointi. '/>
         <Content text='Maatilayrittämiseen liittyvät roolien moninaisuus, työn fyysinen kuormitus, epäsäännöllinen työ ja ajan puute, alan investointivaltaisuus, luonnonolosuhteet sekä tukipolitiikka tuovat omat haasteensa työkykyyn ja sen ylläpitämiseen. Yrityksen on oltava kannattava sekä lyhyellä että pitkällä aikavälillä, joten henkisiä ja fyysisiä voimavaroja tarvitaan jokaiselle päivälle. Erityisen tärkeää on työkyvyn ylläpitäminen pitkäjänteisesti ja siihen vaikuttavia asioita monista eri näkökulmista. Maatilan ja sillä työskentelevien ihmisten hyvinvointi on sitä varmempaa mitä useampi tekijä on kunnossa. (Yliselä 2009.)'/>
-        
-        <img src={OsaAlueet} alt="hyvinvoinnin neljä osa-aluetta" useMap="#puzzle-map"></img>
+        <div className={imageStyle.container}>
+          <img src={OsaAlueet} alt="hyvinvoinnin neljä osa-aluetta" useMap="#puzzle-map"></img>
+        </div>
         <map name="puzzle-map">
             <area onClick={()=> display(<SECTION4_1/>)} alt="fyys" title="fyys"  coords="156,36,151,92,117,86,114,116,145,109,159,157,197,155,204,144,192,127,211,113,230,123,223,134,230,154,271,154,272,121,285,116,313,116,314,87,298,87,281,92,273,39,230,38,225,52,223,82,200,80,192,66,202,48,198,38" shape="poly"></area>
             <area onClick={()=> display(<SECTION4_2/>)} alt="psyyk" title="psyyk"  coords="155,156,156,203,169,212,189,206,198,220,194,234,161,235,152,267,105,268,102,255,108,242,98,229,78,233,80,246,87,265,34,270,28,228,8,233,5,205,30,210,37,153,81,158,78,192,99,197,109,186,101,162,114,155" shape="poly"></area>
