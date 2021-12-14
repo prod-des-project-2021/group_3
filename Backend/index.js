8c3a34a84b51aa7e62c73e87bf53813c1a66b92f
ksoka <t9soka00@students.oamk.fi>
2021-12-14 20:27:52 +0200

    envport

--- Backend/index.js
+++ Backend/index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 4000
+const port = process.env.PORT || 4000
 const client = require('./db');
 const cors = require('cors');
 var bodyParser = require('body-parser')
